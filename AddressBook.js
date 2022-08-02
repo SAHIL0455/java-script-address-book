1fa36034b6a41fc95b1dea7934f9d9bc3859b9e1
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-02 19:47:04 +0530

    ADDED: Ability to create a new address book array and add new contacts to it

--- AddressBook.js
+++ AddressBook.js
@@ -1,19 +1,24 @@
 console.log("Welcome to Addresss Book");
 class Contact {
-  constructor(...params) {
-    this.firstName = params[0];
-    this.lastName = params[1];
-    this.address = params[2];
-    this.city = params[3];
-    this.state = params[4];
-    this.zip = params[5];
-    this.phoneNumber = params[6];
-    this.email = params[7];
+  constructor(firstName,lastName,address,city,state,zip,phoneNumber,email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
   }
   toString() {
     return ("First Name : " +this.firstName +", Last Name : " +this.lastName +", Address : " +this.address +", City : " +this.city +", State : " +this.state +", Zip : " +this.zip + ", Phone Number : " +this.phoneNumber +", Email : " +this.email);
   }
 }
-
-let contact = new Contact("Sahil","Pawar","Chiplun","Ratnagiri","MH",415601,7350310455,"sahilpawar1004@gmail.com");
-console.log(contact.toString());
+let contactList = new Array();
+let contact1 = new Contact("Sahil","Pawar","Chiplun","Ratnagiri","MH",415601,7350310455,"sahilpawar1004@gmail.com");
+let contact2 = new Contact("Sai","Patil","karad","Satara","MH",415901,7350310466,"saipatil1004@gmail.com");
+contactList.push(contact1);
+contactList.push(contact2);
+for (let i = 0; i < contactList.length; i++) {
+    console.log(contactList[i]);   
+}
